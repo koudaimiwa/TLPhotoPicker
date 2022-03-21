2197df339a30afbcafbca0297c1cab7ab33b351a
Koh M <jfygm658@gmail.com>
2022-03-21 15:22:58 +0900

    fixed

--- TLPhotoPicker/Classes/Viewer/UIImage+CenteredFrame.swift
+++ TLPhotoPicker/Classes/Viewer/UIImage+CenteredFrame.swift
@@ -14,7 +14,8 @@ extension UIImage {
             let y = (screenBounds.size.height / 2) - (height / 2)
             centeredFrame = CGRect(x: 0, y: y, width: screenBounds.size.width, height: height)
         } else {
-            let x = (screenBounds.size.width / 2) - ((self.size.width / heightScaleFactor) / 2)
+            var x = (screenBounds.size.width / 2) - ((self.size.width / heightScaleFactor) / 2)
+            x = x < 5 ? 0 : x
             centeredFrame = CGRect(x: x, y: 0, width: screenBounds.size.width - (2 * x), height: screenBounds.size.height)
         }
         return centeredFrame
